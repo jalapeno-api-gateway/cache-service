cb50b59a36b8bf1b3684e232640e8e5a8c71568a
DILLI <illi.dominique.v@gmail.com>
2021-07-16 15:06:17 +0200

    removed comments and adjusted readme

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ type graphDbFeederService struct {
 }
 
 type NodeDocument struct {
-	Key string `json:"_key,omitempty"`;
-	Name string `json:"name,omitempty"`;
-	Asn int32 `json:"asn,omitempty"`;
-	Router_ip string `json:"router_ip,omitempty"`;
+	Key       string `json:"_key,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Asn       int32  `json:"asn,omitempty"`
+	Router_ip string `json:"router_ip,omitempty"`
 }
 
 func newServer() *graphDbFeederService {
@@ -40,14 +40,11 @@ func main() {
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9001: %v", err)
 	}
-	//log.Print("GraphDbFeeder Up and Running")
 }
 
 func (s *graphDbFeederService) GetNodes(nodeIds *graphproto.NodeIds, responseStream graphproto.GraphDbFeeder_GetNodesServer) error {
-	//Access ArangoDB and get Nodes collection
 	arangoDbClient := connectToArangoDb()
 	nodes := getNodesFromArangoDb(arangoDbClient, nodeIds.Ids)
-	//Return stream of Nodes
 	for _, node := range nodes {
 		if err := responseStream.Send(&node); err != nil {
 			log.Fatalf("Could not return node to request-service, %v", err)
@@ -64,7 +61,7 @@ func connectToArangoDb() driver.Client {
 		log.Fatalf("Could not connect to ArangoDb, %v", err)
 	}
 	c, err := driver.NewClient(driver.ClientConfig{
-		Connection: conn,
+		Connection:     conn,
 		Authentication: driver.BasicAuthentication(os.Getenv("ARANGO_DB_USER"), os.Getenv("ARANGO_DB_PASSWORD")),
 	})
 	if err != nil {
@@ -75,39 +72,30 @@ func connectToArangoDb() driver.Client {
 
 func getNodesFromArangoDb(arangoDbClient driver.Client, keys []string) []graphproto.Node {
 	var nodes []graphproto.Node
-	
-	// Opening the database
+
 	ctx := context.Background()
 	db, err := arangoDbClient.Database(ctx, os.Getenv("ARANGO_DB_NAME"))
 	if err != nil {
 		log.Fatalf("Could not open database, %v", err)
 	}
 
-	// Opening LSNode collection
 	col, err := db.Collection(ctx, "LSNode")
 	if err != nil {
 		log.Fatalf("Could not open LSNode collection, %v", err)
 	}
 
 	log.Println("1")
-	for _, key := range keys {		
-		// Reading document from collection
+	for _, key := range keys {
 		var doc NodeDocument
 		_, err := col.ReadDocument(ctx, key, &doc)
 		if err != nil {
-			log.Println("2")
 			log.Fatalf("Could not read document with _id: %s, %v", key, err)
 		}
-
-		log.Println("3")
 		node := graphproto.Node{Key: doc.Key, Name: doc.Name, Asn: doc.Asn, RouterIp: doc.Router_ip}
-		log.Println("4")
 		nodes = append(nodes, node)
 	}
-
 	return nodes
 }
-
 
 // func cacheNode(ctx context.Context, nodeId int, node *rs.NodeResponse) {
 // 	key := strconv.Itoa(nodeId)
