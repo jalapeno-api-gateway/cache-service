2b0370aea538fe45025107fab9463bcb2d7d76df
ILLDOM <illi.dominique.v@gmail.com>
2022-10-20 14:12:21 +0200

    change deleting from cache by using the Id and not the Key. Id contains collection name and values are stored by Id in redis

--- events/eventprocessors.go
+++ events/eventprocessors.go
@@ -16,11 +16,16 @@ func StartEventProcessing() {
 
 	for {
 		select {
-			case event := <-kafka.LsNodeEvents: handleEvent(event, class.LsNode)
-			case event := <-kafka.LsLinkEvents: handleEvent(event, class.LsLink)
-			case event := <-kafka.LsPrefixEvents: handleEvent(event, class.LsPrefix)
-			case event := <-kafka.LsSrv6SidEvents: handleEvent(event, class.LsSrv6Sid)
-			case event := <-kafka.LsNodeEdgeEvents: handleEvent(event, class.LsNodeEdge)
+		case event := <-kafka.LsNodeEvents:
+			handleEvent(event, class.LsNode)
+		case event := <-kafka.LsLinkEvents:
+			handleEvent(event, class.LsLink)
+		case event := <-kafka.LsPrefixEvents:
+			handleEvent(event, class.LsPrefix)
+		case event := <-kafka.LsSrv6SidEvents:
+			handleEvent(event, class.LsSrv6Sid)
+		case event := <-kafka.LsNodeEdgeEvents:
+			handleEvent(event, class.LsNodeEdge)
 		}
 	}
 }
@@ -28,8 +33,8 @@ func StartEventProcessing() {
 func handleEvent(event kafka.KafkaEventMessage, className class.Class) {
 	logrus.WithFields(logrus.Fields{"id": event.Id, "key": event.Key, "action": event.Action}).Trace("Handling incoming Kafka event.")
 	ctx := context.Background()
-	if (event.Action == "del") {
-		redis.DeleteKey(ctx, event.Key)
+	if event.Action == "del" {
+		redis.DeleteKey(ctx, event.Id)
 	} else {
 		id, obj := fetchDocument(ctx, event.Key, className)
 		redis.CacheObject(id, obj)
@@ -38,23 +43,23 @@ func handleEvent(event kafka.KafkaEventMessage, className class.Class) {
 
 func fetchDocument(ctx context.Context, key string, className class.Class) (string, interface{}) {
 	switch className {
-		case class.LsNode:
-			doc := arango.FetchLsNode(ctx, key)
-			return doc.ID, topology.ConvertLsNode(doc)
-		case class.LsLink:
-			doc := arango.FetchLsLink(ctx, key)
-			return doc.ID, topology.ConvertLsLink(doc)
-		case class.LsPrefix:
-			doc := arango.FetchLsPrefix(ctx, key)
-			return doc.ID, topology.ConvertLsPrefix(doc)
-		case class.LsSrv6Sid:
-			doc := arango.FetchLsSrv6Sid(ctx, key)
-			return doc.ID, topology.ConvertLsSrv6Sid(doc)
-		case class.LsNodeEdge:
-			doc := arango.FetchLsNodeEdge(ctx, key)
-			return doc.ID, topology.ConvertLsNodeEdge(doc)
-		default:
-			logrus.WithFields(logrus.Fields{"key": key, "className": className}).Panic("ClassName not implemented.")
+	case class.LsNode:
+		doc := arango.FetchLsNode(ctx, key)
+		return doc.ID, topology.ConvertLsNode(doc)
+	case class.LsLink:
+		doc := arango.FetchLsLink(ctx, key)
+		return doc.ID, topology.ConvertLsLink(doc)
+	case class.LsPrefix:
+		doc := arango.FetchLsPrefix(ctx, key)
+		return doc.ID, topology.ConvertLsPrefix(doc)
+	case class.LsSrv6Sid:
+		doc := arango.FetchLsSrv6Sid(ctx, key)
+		return doc.ID, topology.ConvertLsSrv6Sid(doc)
+	case class.LsNodeEdge:
+		doc := arango.FetchLsNodeEdge(ctx, key)
+		return doc.ID, topology.ConvertLsNodeEdge(doc)
+	default:
+		logrus.WithFields(logrus.Fields{"key": key, "className": className}).Panic("ClassName not implemented.")
 	}
 	return "", nil
 }
